75ae6835f6630b7d30f5f332a3813873a057c29b
Harsha Bharadwaj <88173012+harsha-bharadwaj100@users.noreply.github.com>
2026-04-22 19:47:15 +0530

    commit

---
+++ Bank.java
@@ -0,0 +1,28 @@
+public class Bank {
+    // Bank class implementation, deposit, withdraw.
+
+    // start implementation
+    private double balance;
+
+    public void deposit(double amount) {
+        balance += amount;
+    }
+
+    public void withdraw(double amount) {
+        balance -= amount;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    // end implementation
+
+    public static void main(String[] args) {
+        Bank bank = new Bank();
+        bank.deposit(1000);
+        System.out.println("Balance after deposit: " + bank.getBalance());
+        bank.withdraw(500);
+        System.out.println("Balance after withdrawal: " + bank.getBalance());
+    }
+}
